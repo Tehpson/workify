dfa771862fd42870f190270c3b1d1dbd278a0d8e
Mohammad-khk <mu92.sm11@gmail.com>
2021-06-11 18:04:00 +0200

    Created basc design to signin/register

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,34 +1,15 @@
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-import { HomeView } from '../view/homeview/HomeView'
-import { SignInView } from '../view/signinview/SignInView'
-import { View404 } from '../view/404view/404View'
+import {SignIn} from "../view/signin/SignIn"
 import RoutingPath from './RoutingPath'
-import { useContext, useEffect } from 'react'
-import { UserContext } from '../assets/provider/UserProvider'
-import LocalStorage from '../assets/cache/LocalStorage'
 
 export const Routes = (props: { children?: React.ReactChild }) => {
-	const [authebticatedUser, setAuthebticatedUser] = useContext(UserContext)
-
-	const isUserAuthenticated = () => {
-		setAuthebticatedUser(localStorage.getItem(LocalStorage.username))
-	}
-
-	const blockedRoutesIfAuthenticated = (view: React.FC) => { return authebticatedUser ? HomeView : view }
-
-	const blockedRoutesIfNotAuthenticated = (view: React.FC) => { return !authebticatedUser ? SignInView : view }
-
-	useEffect(() => {
-		isUserAuthenticated()
-	})
 
 	return (
 		<BrowserRouter>
 			{props.children}
 			<Switch>
-				<Route exact path={RoutingPath.signInView} component={blockedRoutesIfAuthenticated(SignInView)} />
-				<Route exact path={RoutingPath.homeView} component={blockedRoutesIfNotAuthenticated(HomeView)} />
-				<Route component={View404} />
+				<Route exact path={RoutingPath.signIn} component={SignIn} />
+				<Route component={SignIn} />
 			</Switch>
 		</BrowserRouter>
 	)
---
+++ src/view/register/Register.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Wrapper } from '../register/Register.Style';
+
+export const Register = () => {
+  return (
+    <Wrapper>
+      <div>
+        <h1>Register</h1>
+      </div>
+    </Wrapper>
+  );
+};
