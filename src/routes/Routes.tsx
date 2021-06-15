9dfa97bcac65a2c568309162b6aba1004a488fa0
Mohammad Al Khano <Mu92.sm11@gmail.com>
2021-06-15 11:22:18 +0200

    resolved merge conflicts

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -8,14 +8,8 @@ export const Routes = (props: { children?: React.ReactChild }) => {
 		<BrowserRouter>
 			{props.children}
 			<Switch>
-<<<<<<< HEAD
 				<Route exact path={RoutingPath.signIn} component={SignIn} />
 				<Route component={SignIn} />
-=======
-				<Route exact path={RoutingPath.signInView} component={(SignInView)} />
-				<Route exact path={RoutingPath.homeView} component={(HomeView)} />
-				<Route component={View404} />
->>>>>>> 591f2753f66fe77be6f54b3f66c3da2deab38985
 			</Switch>
 		</BrowserRouter>
 	)
