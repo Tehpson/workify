24e90261a93cd793b411882b4802d58c98d587d8
Mohammad Al Khano <Mu92.sm11@gmail.com>
2021-06-15 17:50:08 +0200

    edited form size (BoxContainer)

--- src/components/accountBox/index.tsx
+++ src/components/accountBox/index.tsx
@@ -6,7 +6,7 @@ import { AccountContext } from './accountContext';
 import { SignupForm } from './signupForm';
 
 const BoxContainer = styled.div`
-  width: 380px;
+  width: 680px;
   min-height: 550px;
   display: grid | inline-grid;
   grid-template-columns: 100px 50px 100px;
@@ -35,7 +35,7 @@ const BackDrop = styled(motion.div)`
   flex-direction: column;
   border-radius: 50%;
   transform: rotate(60deg);
-  top: -290px;
+  top: -520px;
   left: -70px;
   background: rgb(241, 196, 15);
   background: linear-gradient(
@@ -47,7 +47,6 @@ const BackDrop = styled(motion.div)`
 
 const HeaderContainer = styled.div`
   width: 100%;
-
   display: flex;
   flex-direction: column;
 `;
@@ -68,6 +67,7 @@ const SmallText = styled.h5`
   z-index: 10;
   margin: 0;
   margin-top: 7px;
+  
 `;
 
 const InnerContainer = styled.div`
@@ -96,7 +96,7 @@ const backdropVariants = {
 const expandingTransition = {
   type: 'spring',
   duration: 2.3,
-  stiffness: 30,
+  stiffness: 40,
 };
 
 export function AccountBox(props: any) {
@@ -114,14 +114,14 @@ export function AccountBox(props: any) {
     playExpandingAnimation();
     setTimeout(() => {
       setActive('signup');
-    }, 400);
+    }, 100);
   };
 
   const switchToSignin = () => {
     playExpandingAnimation();
     setTimeout(() => {
       setActive('signin');
-    }, 400);
+    }, 100);
   };
 
   const contextValue = { switchToSignup, switchToSignin };
@@ -140,6 +140,7 @@ export function AccountBox(props: any) {
             <HeaderContainer>
               <HeaderText>Welcome</HeaderText>
               <HeaderText>To Workify</HeaderText>
+              <SmallText >Make training more social</SmallText>
               <SmallText>Please sign-in to continue!</SmallText>
             </HeaderContainer>
           )}
