84525e565fe65a36116989db669a875d98bbe1e3
Marcus Sjöholm <marcusasjoholm93@gmail.com>
2021-06-16 13:02:10 +0200

    Changed avatar and positioned stuff

--- src/view/home/HomeView.tsx
+++ src/view/home/HomeView.tsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import './HomeView.css'
+import Avatar from '../../assets/image/avatar.png'
 
 export const Index = () => {
     return (
         <div className='homeViewWrapper'>
             <img
                 className='ProfilePicture'
-                src='https://thispersondoesnotexist.com/image'
+                src={Avatar}
                 alt='error..' />
             <span className='userName'>User Name</span>
             <span className='bio'>Bio</span>
-            <textarea className='addNewWorkout'>Enter workout</textarea>
+            <button className='postButton'>Post</button>
         </div>
 
 
