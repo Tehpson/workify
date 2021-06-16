c31775c130394475b8a0be961700bd00fb5384da
Mohammad Al Khano <Mu92.sm11@gmail.com>
2021-06-16 13:49:51 +0200

    Edited UseEffect and displayed data

--- src/view/home/homeView.tsx
+++ src/view/home/homeView.tsx
@@ -1,41 +1,41 @@
-import React from 'react'
-import './HomeView.css'
-import Avatar from '../../assets/image/avatar.png'
-import { useEffect } from 'react'
-import WorkifyAPIService from '../../assets/api/service/WorkifyAPIService'
-import {UserContext} from '../../provider/UserProvider'
-import {useContext, useState} from 'react'
+import React from 'react';
+import './HomeView.css';
+import Avatar from '../../assets/image/avatar.png';
+import { useEffect } from 'react';
+import WorkifyAPIService from '../../assets/api/service/WorkifyAPIService';
+import { UserContext } from '../../provider/UserProvider';
+import { useContext, useState } from 'react';
 
 export const Index = () => {
-	const [authebticatedUser, setAuthebticatedUser] = useContext(UserContext)
-	const [serverRespons, setServerRespons] = useState<any>(null)
+  const [authebticatedUser, setAuthebticatedUser] = useContext(UserContext);
+  const [serverRespons, setServerRespons] = useState<any>(null);
+  
 
-    useEffect(() => {
-        
-    }, [])
+  useEffect(() => {
+    fetchWorkoutData();
+  }, []);
 
-    const fetchWorkoutData = async () => {
-        try {
-            const {data} = await WorkifyAPIService.GetallWorkouts(authebticatedUser)
-			setServerRespons(data)
-            
-        }
-        catch (error){
-			console.log(error)
-        }
+  const fetchWorkoutData = async () => {
+    try {
+      const { data } = await WorkifyAPIService.GetallWorkouts(
+        authebticatedUser
+      );
+      setServerRespons(data);
+    } catch (error) {
+      console.log(error);
     }
+  };
+  const displayData = ()=>{
+      serverRespons.results.map
+  }
 
-    return (
-        <div className='homeViewWrapper'>
-            <img
-                className='ProfilePicture'
-                src={Avatar}
-                alt='error..' />
-            <span className='userName'>User Name</span>
-            <span className='bio'>Bio</span>
-            <button className='postButton'>Post</button>
-        </div>
-
-
-    )
-}
+  return (
+      
+    <div className="homeViewWrapper">
+      <img className="ProfilePicture" src={Avatar} alt="error.." />
+      <span className="userName">User Name</span>
+      <span className="bio">Bio</span>
+      <button className="postButton">Post</button>
+    </div>
+  );
+};
