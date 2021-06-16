03c42a11458ae000af4febd7c1f05a2b234539c6
AlbinJGitHub <72981304+AlbinJGitHub@users.noreply.github.com>
2021-06-16 10:29:00 +0200

    yeetusfeetus

--- src/components/CreateWorkout/Layout1.tsx
+++ src/components/CreateWorkout/Layout1.tsx
@@ -4,11 +4,11 @@ export const Layout1 = () => {
 	return (
 		<body className="body">
 			<div className="container w-container">
-				<h1 className="heading">Username</h1>
-				<p className="post">awdaagregsergesrgfsedrf</p>
-				<h1 className="heading-2">Title for Post</h1>
-				<h1 className="heading-3">15 min</h1>
-				<div className="timeofpost">time/date</div>
+				<h1 className="heading">Tehpson</h1>
+				<p className="post">mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm</p>
+				<h1 className="heading-2">Löpning</h1>
+				<h1 className="heading-3">60 min</h1>
+				<div className="timeofpost">10:22AM - 6/16/21</div>
 			</div>
 		</body>
 	)
