41ed53400975b1a661914d216b2007acbd9b2617
Marcus Sjöholm <marcusasjoholm93@gmail.com>
2021-06-16 13:32:57 +0200

    Added a function

--- src/view/home/homeView.tsx
+++ src/view/home/homeView.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import './HomeView.css'
 import Avatar from '../../assets/image/avatar.png'
+import { useEffect } from 'react'
+import WorkifyAPIService from '../../assets/api/service/WorkifyAPIService'
 
 export const Index = () => {
+    useEffect(() => {
+        
+    }, [])
+
+    const fetchWorkoutData = async () => {
+        try {
+            const data = await {
+                
+            }
+        }
+        catch {
+
+        }
+    }
+
     return (
         <div className='homeViewWrapper'>
             <img
