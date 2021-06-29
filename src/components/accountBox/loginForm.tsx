edc6670cf9c9e5deee12479a11d7e9f345a5ebbe
Tehpson <jesper@familjen-pettersson.se>
2021-06-29 18:36:52 +0200

    idk

--- src/components/accountBox/loginForm.tsx
+++ src/components/accountBox/loginForm.tsx
@@ -1,70 +1,84 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import WorkifyAPIService from '../../assets/api/service/WorkifyAPIService'
 import {
-  BoldLink,
-  BoxContainer,
-  FormContainer,
-  Input,
-  MutedLink,
-  SubmitButton,
+	BoldLink,
+	BoxContainer,
+	FormContainer,
+	Input,
+	MutedLink,
+	SubmitButton,
 } from './Style';
 import { Marginer } from '../marginer';
 import { AccountContext } from './accountContext';
 import { useHistory } from 'react-router-dom';
 import RoutingPath from '../../routes/RoutingPath';
 export const LoginForm = (props: any) => {
-  const userHistory = useHistory();
-  const [credentials, setCredentials] = useState({ email: '', password: '' });
-  const { switchToSignup } = useContext(AccountContext);
-  const Login = () => {
-    WorkifyAPIService.RequestLogin(credentials.email, credentials.password)
-    userHistory.push(RoutingPath.homeView);
-  }
-  // const state = {email: "",password: ""};
-  return (
-    <BoxContainer>
-      <FormContainer>
-        <Input
-          type="email"
-          value={credentials.email}
-          placeholder="Please enter your e-mail address"
-          onChange={(event) =>
-            setCredentials({
-              email: event.target.value,
-              password: credentials.password,
-            })
-          }
-        />
-        <Input
-          value={credentials.password}
-          type="password"
-          placeholder="enter your password"
-          onChange={(event) =>
-            setCredentials({
-              email: credentials.email,
-              password: event.target.value,
-            })
-          }
-        />
-      </FormContainer>
-      <Marginer direction="vertical" margin={10} />
-      <MutedLink href="#">Forget your password?</MutedLink>
-      <Marginer direction="vertical" margin="1.6em" />
-      <SubmitButton
-        type="submit"
-        onClick={() => Login()}
-      >
-        Signin
-      </SubmitButton>
-      <Marginer direction="vertical" margin="1em" />
-      <MutedLink href="#">
-        Don't have an accoun?
-        <BoldLink href="#" onClick={switchToSignup}>
-          Signup
-        </BoldLink>
-      </MutedLink>
-    </BoxContainer>
-  );
+	const history = useHistory();
+	const [credentials, setCredentials] = useState({ email: '', password: '' });
+	const { switchToSignup } = useContext(AccountContext);
+	const [serverResponse, setServerResponse] = useState<any>()
+	const [errorMSG, setErrorMSG] = useState("")
+
+	const Login = async () => {
+		try {
+			setServerResponse(await WorkifyAPIService.RequestLogin(credentials.email, credentials.password))
+
+		} catch (error) {
+			console.log(error)
+			setErrorMSG(error?.status)
+		}
+	}
+
+
+	useEffect(() => {
+		history.push(RoutingPath.homeView)
+	}, [serverResponse])
+
+	return (
+		<BoxContainer>
+			<span>{errorMSG}</span>
+			<FormContainer>
+				<Input
+					type="email"
+					value={credentials.email}
+					placeholder="Please enter your e-mail address"
+					onChange={(event) =>
+						setCredentials({
+							email: event.target.value,
+							password: credentials.password,
+						})
+					}
+				/>
+				<Input
+					value={credentials.password}
+					type="password"
+					placeholder="enter your password"
+					onChange={(event) =>
+						setCredentials({
+							email: credentials.email,
+							password: event.target.value,
+						})
+					}
+				/>
+			</FormContainer>
+			<Marginer direction="vertical" margin={10} />
+			<MutedLink href="#">Forget your password?</MutedLink>
+			<Marginer direction="vertical" margin="1.6em" />
+			<SubmitButton
+				type="submit"
+				onClick={() => Login()}
+			>
+				Signin
+			</SubmitButton>
+			<Marginer direction="vertical" margin="1em" />
+			<MutedLink href="#">
+				Don't have an accoun?
+				<BoldLink href="#" onClick={switchToSignup}>
+					Signup
+				</BoldLink>
+			</MutedLink>
+		</BoxContainer>
+	);
 };
 
 
